0bf9e39d963512399117b823d1d8c3abea43ffd2
huangyu <598050554@qq.com>
2022-04-29 10:30:40 +0800

    add

--- struct/struct_02.c
+++ struct/struct_02.c
@@ -4,20 +4,34 @@
 
 #include <stdio.h>
 #include <malloc.h>
+#include <string.h>
 
 struct Students {
     int age;
-    float score;
     char sex;
+    float score;
+    char name[60];
 };
 
+void InputStudent(struct Students *students);
+
+void OutputStudent(const struct Students *students);
+
 int main() {
-
-    int i;
-    struct Students stu[5];
-    for (i = 0; i < 5; ++i) {
-        scanf("age: %d", &stu[i].age);
-    }
-
+    struct Students students;
+    printf("%llu\n", sizeof(students));
+    InputStudent(&students);
+    OutputStudent(&students);
     return 0;
+}
+
+void InputStudent(struct Students *students) {
+    students->age = 10;
+    strcpy(students->name, "liSi");
+    students->score = 98.2F;
+    students->sex = 'F';
+}
+
+void OutputStudent(const struct Students *students) {
+    printf("name: %s\nage: %d\nscore: %.2f\nsex: %c\n", students->name, students->age, students->score, students->sex);
 }
